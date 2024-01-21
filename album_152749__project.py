1dbbfeeb3f7d4b6b58652ff1a5d3088ac6416531
Atorami <atoramiseo@mail.ru>
2024-01-21 21:52:36 +0100

    fix: algorithms fix

--- album_152749__project.py
+++ album_152749__project.py
@@ -28,48 +28,53 @@ def script_3():
 
 
 def scene_1(data):
+    repeat_num, floor_height, scripts = data
     scene_data = []
 
-    for i in data[2]:
+    for i in range(len(scripts)):
         floor_counter = 0
         last_floor = 0
 
-        for j in range(data[0]):
-            start, finish = i
+        for _ in range(repeat_num):
+            start, finish = scripts[i]()
             lift_back = abs(last_floor - start)
             lift_next = abs(start - finish)
             floor_counter += (lift_back + lift_next)
             last_floor = finish
 
-        scene_data.append(round(floor_counter * data[1] / 1000, 2))
+        scene_data.append(round(floor_counter * floor_height / 1000, 2))
     return scene_data
 
 
 def scene_2(data):
+    repeat_num, floor_height, scripts = data
     scene_data = []
-    current_floor = 0
 
-    for i in data[2]:
+    for i in range(len(scripts)):
         floor_counter = 0
-        for j in range(data[0]):
-            start, finish = i
-            floor_counter += abs(current_floor - start) + abs(start - finish) + finish
-            current_floor = 0
-        scene_data.append(round(floor_counter * data[1] / 1000, 2))
+
+        for _ in range(repeat_num):
+            start, finish = scripts[i]()
+            floor_counter += start + abs(start - finish) + finish
+
+        scene_data.append(round(floor_counter * floor_height / 1000, 2))
 
     return scene_data
 
 
-# elevator back to middle of the building after each lifting
 def scene_3(data):
+    repeat_num, floor_height, scripts = data
     scene_data = []
-    for i in data[2]:
+    middle_floor = 5
+
+    for i in range(len(scripts)):
         floor_counter = 0
-        middle_floor = 5
-        for j in range(data[0]):
-            start, finish = i
-            floor_counter += abs(start - finish) + abs(finish - middle_floor)
-        scene_data.append(round(floor_counter * data[1] / 1000, 2))
+
+        for _ in range(repeat_num):
+            start, finish = scripts[i]()
+            floor_counter += abs(start - middle_floor) + abs(start - finish) + abs(finish - middle_floor)
+
+        scene_data.append(round(floor_counter * floor_height / 1000, 2))
 
     return scene_data
 
@@ -77,17 +82,20 @@ def scene_3(data):
 def main():
     repeat_num = 1000
     floor_height = 2.8
-    scene_input_data = [repeat_num, floor_height, [script_1(), script_2(), script_3()]]
-    scene_1_data = scene_1(scene_input_data)
-    scene_2_data = scene_2(scene_input_data)
-    scene_3_data = scene_3(scene_input_data)
+    scripts = [script_1, script_2, script_3]
+    data = [repeat_num, floor_height, scripts]
+
+    scene_1_data = scene_1(data)
+    scene_2_data = scene_2(data)
+    scene_3_data = scene_3(data)
+
     scene_output_data = [scene_1_data, scene_2_data, scene_3_data]
 
     headers = ["Algorithm I", "Algorithm II", "Algorithm III"]
     table_data = []
 
     for i in range(len(scene_output_data)):
-        row_data = [f"Scene {i+1}"]
+        row_data = [f"Scene {i + 1}"]
         for j in range(len(scene_output_data[i])):
             row_data.append(f"{scene_output_data[i][j]:.2f} km")
         table_data.append(row_data)
